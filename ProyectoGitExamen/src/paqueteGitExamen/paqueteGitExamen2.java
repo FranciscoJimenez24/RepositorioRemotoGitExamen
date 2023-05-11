a47cc1a2ecb42d52750430e7933cc260d0236350
paco <paco@gmail.com>
2023-05-11 20:47:09 +0200

    examenGIT2

---
+++ ProyectoGitExamen/src/paqueteGitExamen/paqueteGitExamen2.java
@@ -0,0 +1,11 @@
+package paqueteGitExamen;
+
+public class paqueteGitExamen2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Rama 1");
+		System.out.println("Rama 2");
+	}
+
+}
